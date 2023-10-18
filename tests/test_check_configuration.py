a7afe30c31c1e1e336c618e237f9c9acd73b01be
Philippe Mallet-Ladeira <philippe.mallet-ladeira@csgroup.eu>
2023-10-18 10:47:12 +0200

    tests: refact check_configuration to pytest style

--- tests/test_check_configuration.py
+++ tests/test_check_configuration.py
@@ -23,130 +23,110 @@
 """
 Test configuration
 """
-import unittest
-from typing import Any, Dict
 
 import pytest
 import transitions
 from json_checker import DictCheckerError
 
 from pandora2d import check_configuration
-from pandora2d.state_machine import Pandora2DMachine
 from tests import common
 
 
-class TestCheckJson(unittest.TestCase):
+class TestCheckInputSection:
     """
-    TestCheckJson class allows to test all the methods in the class CheckJson
+    Test check input section.
     """
 
-    def setUp(self) -> None:
-        """
-        Method called to prepare the test fixture
-
-        """
-
-    @staticmethod
-    def test_check_input_section() -> None:
-        """
-        Test function for checking user input section
-        """
-
+    def test_check_nominal_case(self) -> None:
         assert check_configuration.check_input_section(common.correct_input)
 
+    def test_false_input_disp_should_exit(self):
         with pytest.raises(SystemExit):
             check_configuration.check_input_section(common.false_input_disp)
+
+    def test_false_input_path_image_should_raise_error(self):
         with pytest.raises(DictCheckerError):
             check_configuration.check_input_section(common.false_input_path_image)
 
-    @staticmethod
-    def test_check_pipeline_section() -> None:
+
+class TestCheckPipelineSection:
+    """Test check_pipeline_section."""
+
+    def test_nominal_case(self, pandora2d_machine) -> None:
         """
         Test function for checking user pipeline section
         """
-
-        pandora2d_machine = Pandora2DMachine()
-
         assert check_configuration.check_pipeline_section(common.correct_pipeline_dict, pandora2d_machine)
 
+    def test_false_mc_dict_should_raise_error(self, pandora2d_machine):
         with pytest.raises(transitions.core.MachineError):
             check_configuration.check_pipeline_section(common.false_pipeline_mc_dict, pandora2d_machine)
+
+    def test_false_disp_dict_should_raise_error(self, pandora2d_machine):
         with pytest.raises(transitions.core.MachineError):
             check_configuration.check_pipeline_section(common.false_pipeline_disp_dict, pandora2d_machine)
 
-    @staticmethod
-    def test_check_roi_section() -> None:
+
+class TestCheckRoiSection:
+    """Test check_roi_section."""
+
+    def test_nominal_case(self) -> None:
         """
         Test function for checking user ROI section
         """
-
         # with a correct ROI check_roi_section should return nothing
         assert check_configuration.check_roi_section(common.correct_ROI_sensor)
 
-        # if a dimension < 0 check_roi_section should raise BaseException error
+    def test_dimension_lt_0_raises_exception(self):
         with pytest.raises(BaseException):
             check_configuration.check_roi_section(common.false_ROI_sensor_negative)
-        # if a dimension first > last check_roi_section should raise BaseException error
+
+    def test_first_dimension_gt_last_dimension_raises_exception(self):
         with pytest.raises(BaseException):
             check_configuration.check_roi_section(common.false_ROI_sensor_first_superior_to_last)
 
-    @staticmethod
-    def test_get_roi_pipeline() -> None:
-        """
-        Test get_roi_pipeline function
-        """
 
-        assert common.correct_ROI_sensor == check_configuration.get_roi_config(common.correct_ROI_sensor)
+def test_get_roi_pipeline() -> None:
+    """
+    Test get_roi_pipeline function
+    """
+    assert common.correct_ROI_sensor == check_configuration.get_roi_config(common.correct_ROI_sensor)
 
-    @staticmethod
-    def test_check_roi_coherence() -> None:
-        """
-        Test check_roi_coherence function
-        """
 
-        # if first < last check_roi_coherence should return None
+class TestCheckRoiCoherence:
+    """Test check_roi_coherence."""
+
+    def test_first_lt_last_is_ok(self) -> None:
         assert check_configuration.check_roi_coherence(common.correct_ROI_sensor["ROI"]["col"]) is None
 
-        # if first > last check_roi_coherence should raise SystemExit error
+    def test_first_gt_last_raises_error(self):
         with pytest.raises(SystemExit):
             check_configuration.check_roi_coherence(common.false_ROI_sensor_first_superior_to_last["ROI"]["col"])
 
-    def test_check_step(self) -> None:
+
+class TestCheckStep:
+    """Test check_step."""
+
+    def test_nominal_case(self, pipeline_config, pandora2d_machine) -> None:
         """
         Test step configuration with user configuration dictionary
         """
-        pandora2d_machine = Pandora2DMachine()
-
-        pipeline_cfg: Dict[str, Dict[str, Any]] = common.correct_pipeline_dict
 
         # Add correct step
-        pipeline_cfg["pipeline"]["matching_cost"]["step"] = [1, 1]
-        assert check_configuration.check_pipeline_section(pipeline_cfg, pandora2d_machine)
+        pipeline_config["pipeline"]["matching_cost"]["step"] = [1, 1]
+        assert check_configuration.check_pipeline_section(pipeline_config, pandora2d_machine)
 
-        pandora2d_machine = Pandora2DMachine()
-
-        # Test with a one size list step : test should fail
-        pipeline_cfg["pipeline"]["matching_cost"]["step"] = [1]
+    @pytest.mark.parametrize(
+        "step",
+        [
+            pytest.param([1], id="one size list"),
+            pytest.param([-1, 1], id="negative value"),
+            pytest.param([1, 1, 1], id="More than 2 elements"),
+            pytest.param([1, "1"], id="String element"),
+        ],
+    )
+    def test_fails_with_bad_step_values(self, pipeline_config, pandora2d_machine, step) -> None:
+        """Test check_pipeline_section fails with bad values of step."""
+        pipeline_config["pipeline"]["matching_cost"]["step"] = step
         with pytest.raises(DictCheckerError):
-            check_configuration.check_pipeline_section(pipeline_cfg, pandora2d_machine)
-
-        pandora2d_machine = Pandora2DMachine()
-
-        # Test with a negative step : test should fail
-        pipeline_cfg["pipeline"]["matching_cost"]["step"] = [-1, 1]
-        with pytest.raises(DictCheckerError):
-            check_configuration.check_pipeline_section(pipeline_cfg, pandora2d_machine)
-
-        pandora2d_machine = Pandora2DMachine()
-
-        # Test with a three elements list step : test should fail
-        pipeline_cfg["pipeline"]["matching_cost"]["step"] = [1, 1, 1]
-        with pytest.raises(DictCheckerError):
-            check_configuration.check_pipeline_section(pipeline_cfg, pandora2d_machine)
-
-        pandora2d_machine = Pandora2DMachine()
-
-        # Test with a str elements list step : test should fail
-        pipeline_cfg["pipeline"]["matching_cost"]["step"] = [1, "1"]
-        with pytest.raises(DictCheckerError):
-            check_configuration.check_pipeline_section(pipeline_cfg, pandora2d_machine)
+            check_configuration.check_pipeline_section(pipeline_config, pandora2d_machine)
