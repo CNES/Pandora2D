0ca65bb1d42c0b65dd1082850bf8b6682ac26d3e
Alice de Bardonnèche-Richard <alice.de-bardonneche-richard@csgroup.eu>
2024-01-22 16:32:52 +0100

    test: add cfg in run_prepare API

--- tests/test_pandora2d.py
+++ tests/test_pandora2d.py
@@ -74,10 +74,11 @@ class TestPandora2D:
         }
         img_left, img_right = create_datasets_from_inputs(input_config=input_config)
 
-        pandora2d_machine.run_prepare(img_left, img_right)
+        pandora2d_machine.run_prepare(img_left, img_right, input_config)
 
         assert pandora2d_machine.left_img == img_left
         assert pandora2d_machine.right_img == img_right
+        assert pandora2d_machine.completed_cfg == input_config
         np.testing.assert_array_equal(
             pandora2d_machine.disp_min_col, np.full((img_left.sizes["row"], img_left.sizes["col"]), -2)
         )
