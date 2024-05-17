0505e1a4d191e7f344e7ca93227a1863abac5c7c
duzelis <elisa.duz@csgroup.eu>
2024-05-17 17:07:45 +0200

    feat: add interpolate method

--- pandora2d/interpolation_filter/interpolation_filter.py
+++ pandora2d/interpolation_filter/interpolation_filter.py
@@ -21,9 +21,10 @@ This module contains functions associated to the interpolation filters.
 """
 from __future__ import annotations
 import logging
-from typing import Dict
+from typing import Dict, Tuple, List
 from abc import abstractmethod, ABC
 
+import math
 import numpy as np
 
 from pandora.margins.descriptors import NullMargins
@@ -38,6 +39,7 @@ class AbstractFilter(ABC):
     _interpolation_filter_method = None
     cfg = None
     margins = NullMargins()
+    _SIZE = 4
 
     def __new__(cls, filter_method: str | None = None):
         """
@@ -115,3 +117,61 @@ class AbstractFilter(ABC):
         """
 
         return (row_coeff.dot(resampling_area)).dot(col_coeff)
+
+    def interpolate(
+        self, image: np.ndarray, positions: Tuple[np.ndarray, np.ndarray], max_fractional_value: float = 0.998046875
+    ) -> List:
+        """
+        Returns the values of the 8 interpolated position
+
+        :param image: image
+        :type image: np.ndarray
+        :param positions: subpix positions to be interpolated
+        :type positions: Tuple[np.ndarray, np.ndarray]
+        :param max_fractional_value: maximum fractional value used to get coefficients
+        :type max_fractional_value: float
+        :return: the interpolated values of the corresponding subpix positions
+        :rtype: List of float
+        """
+
+        # Initialisation of the result list
+        interpolated_positions = []
+
+        # Epsilon machine
+        eps = np.finfo(np.float32).eps
+
+        for pos_col, pos_row in zip(*positions):
+
+            # get_coeffs method receives positive coefficients
+            fractional_row = abs(math.modf(pos_row)[0])
+            fractional_col = abs(math.modf(pos_col)[0])
+
+            # If the subpixel shift is too close to 1, max_fractional_value is returned to avoid rounding.
+            if 1 - fractional_row < eps:
+                fractional_row = max_fractional_value
+            if 1 - fractional_col < eps:
+                fractional_col = max_fractional_value
+
+            # Get interpolation coefficients for fractional_row and fractional_col shifts
+            coeffs_row = self.get_coeffs(fractional_row)
+            coeffs_col = self.get_coeffs(fractional_col)
+
+            # Computation of the top left point of the resampling area
+            # on which the interpolating coefficients will be applied with apply method
+            # In cost_surface, row dimension is disp_col and column dimension is disp_row,
+            # then we use margins.left for row and margins.up for col
+            top_left_area_row = int(np.floor(pos_row) - self.margins.left)
+            top_left_area_col = int(np.floor(pos_col) - self.margins.up)
+
+            # Resampling area to which we will apply the interpolator coefficients
+            resampling_area = image[
+                top_left_area_row : top_left_area_row + self._SIZE, top_left_area_col : top_left_area_col + self._SIZE
+            ]
+
+            # Application of the interpolator coefficients on resampling area
+            interpolated_value = self.apply(resampling_area, coeffs_row, coeffs_col)
+
+            # Add new interpolated value in the result list
+            interpolated_positions.append(interpolated_value)
+
+        return interpolated_positions
