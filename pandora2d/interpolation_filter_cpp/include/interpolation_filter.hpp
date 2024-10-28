c3df94772acab4e0aeff4c28733b54cbb0ab4c5d
Alice de Bardonnèche-Richard <alice.de-bardonneche-richard@csgroup.eu>
2024-10-28 11:48:01 +0100

    feat: interpolation_filer cpp files

---
+++ pandora2d/interpolation_filter_cpp/include/interpolation_filter.hpp
@@ -0,0 +1,94 @@
+/* Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+ *
+ * This file is part of PANDORA2D
+ *
+ *     https://github.com/CNES/Pandora2D
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+This module contains functions associated to the Abstract filter class for cpp.
+*/
+
+#ifndef INTERPOLATIONFILTER_HPP
+#define INTERPOLATIONFILTER_HPP
+
+#include "common.hpp"
+#include <Eigen/Dense>
+
+namespace abstractfilter
+{
+
+    /**
+     * @brief  This abstract class allows for the instantiation of a filter.
+     */
+
+    class AbstractFilter
+    {
+    public:
+        /**
+         * @brief Construct a new Abstract Filter object
+         * 
+         */
+        AbstractFilter();
+        /**
+         * @brief Destroy the Abstract Filter object
+         * 
+         */
+        virtual ~AbstractFilter();
+
+        /**
+         * @brief Get the coeffs object
+         * 
+         * @param fractional_shift positive fractional shift of the subpixel 
+         * position to be interpolated
+         * @return t_Vector, an array of interpolator coefficients 
+         * whose size depends on the filter margins
+         */
+        virtual t_Vector get_coeffs(const double &fractional_shift);
+
+        /**
+         * @brief  Returns the value of the interpolated position
+         * 
+         * @param resampling_area area on which interpolator coefficients will be applied
+         * @param row_coeff interpolator coefficients in cols
+         * @param col_coeff interpolator coefficients in rows
+         * @return double 
+         */
+        double apply(const t_Matrix &resampling_area,
+                    const t_Vector &row_coeff,
+                    const t_Vector &col_coeff) const;
+
+        /**
+         * @brief 
+         * 
+         * @param image image
+         * @param col_positions subpix columns positions to be interpolated
+         * @param row_positions subpix rows positions to be interpolated
+         * @param max_fractional_value maximum fractional value used to get coefficients
+         * @return t_Vector, the interpolated value of the position 
+         * corresponding to col_coeff and row_coeff
+         */
+        t_Vector interpolate(const t_Matrix &image,
+                             const t_Vector &col_positions,
+                             const t_Vector &row_positions,
+                             const double max_fractional_value = 0.998046875);
+
+    protected:
+        int m_size = 4;
+        Margins m_margins{0, 0, 0, 0};
+    };
+}
+
+#endif // namespace filter
---
+++ pandora2d/interpolation_filter_cpp/src/interpolation_filter.cpp
@@ -0,0 +1,119 @@
+/* Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+ *
+ * This file is part of PANDORA2D
+ *
+ *     https://github.com/CNES/Pandora2D
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+This module contains functions associated to the Abstract filter class for cpp.
+*/
+
+#include "../include/interpolation_filter.hpp"
+#include <iostream>
+#include <limits>
+#include <cmath>
+
+namespace abstractfilter
+{
+
+    // Constructor
+    AbstractFilter::AbstractFilter()
+        : m_size(4),
+          m_margins({0, 0, 0, 0})
+    {
+    }
+
+    // Destructor
+    AbstractFilter::~AbstractFilter()
+    {
+    }
+
+    t_Vector AbstractFilter::get_coeffs(const double &fractional_shift)
+    {
+        return t_Vector();
+    }
+
+    // Apply
+    double AbstractFilter::apply(const t_Matrix &resampling_area,
+                                const t_Vector &row_coeff,
+                                const t_Vector &col_coeff) const
+    {
+
+        t_Vector intermediate_result = resampling_area * col_coeff;
+        return row_coeff.dot(intermediate_result);
+    }
+
+    // Interpolate
+    t_Vector AbstractFilter::interpolate(const t_Matrix &image,
+                                         const t_Vector &col_positions,
+                                         const t_Vector &row_positions,
+                                         const double max_fractional_value)
+    {
+
+        // Initialisation of the result list
+        t_Vector interpolated_positions = t_Vector::Zero(col_positions.size());
+
+        // Epsilon machine
+        const double eps = std::numeric_limits<double>::epsilon();
+
+        // AbstractFilter
+        const Margins &my_margins = AbstractFilter::m_margins;
+        const int filter_size = AbstractFilter::m_size;
+
+        for (int i = 0; i < col_positions.size(); ++i)
+        {
+            // get_coeffs method receives positive coefficients
+            int pos_col = col_positions[i];
+            int pos_row = row_positions[i];
+
+            double fractional_row = pos_row - std::floor(pos_row);
+            double fractional_col = pos_col - std::floor(pos_col);
+
+            // If the subpixel shift is too close to 1, max_fractional_value is returned to avoid rounding.
+            if (1 - fractional_row < eps)
+            {
+                fractional_row = max_fractional_value;
+            }
+            if (1 - fractional_col < eps)
+            {
+                fractional_col = max_fractional_value;
+            }
+
+            // Get interpolation coefficients for fractional_row and fractional_col shifts
+            t_Vector coeffs_row = this->get_coeffs(fractional_row);
+            t_Vector coeffs_col = this->get_coeffs(fractional_col);
+
+            /*
+            Computation of the top left point of the resampling area
+            on which the interpolating coefficients will be applied with apply method
+            In cost_surface, row dimension is disp_col and column dimension is disp_row,
+            then we use margins.left for row and margins.up for col
+            */
+            int top_left_area_row = pos_row - my_margins.left;
+            int top_left_area_col = pos_col - my_margins.up;
+
+            // Resampling area to which we will apply the interpolator coefficients
+            t_Matrix resampling_area = image.block(top_left_area_row, top_left_area_col, filter_size, filter_size);
+
+            // Application of the interpolator coefficients on resampling area
+            const auto result = apply(resampling_area, coeffs_row, coeffs_col);
+            interpolated_positions[i] = result;
+        }
+
+        return interpolated_positions;
+    }
+
+} // namespace Abstractfilter
