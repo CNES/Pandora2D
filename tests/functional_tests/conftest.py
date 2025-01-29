2a49f571c8361979efd5643909f049d18e175a7e
duzelis <elisa.duz@cs-soprasteria.com>
2025-01-29 10:49:15 +0100

    test: move georeferencement fixtures to conftest

--- tests/functional_tests/conftest.py
+++ tests/functional_tests/conftest.py
@@ -18,7 +18,13 @@
 #
 """Module with global test fixtures."""
 
+# pylint: disable=redefined-outer-name
+
+import rasterio
+import xarray as xr
+import numpy as np
 import pytest
+from pandora.common import write_data_array
 
 
 @pytest.fixture()
@@ -69,4 +75,80 @@ def correct_input_for_functional_tests(left_img_path, right_img_path, col_dispar
             "col_disparity": col_disparity,
             "row_disparity": row_disparity,
         }
+    }
+
+
+@pytest.fixture()
+def left_data():
+    """Create left data."""
+    # Create array of shape (10,10):
+    # [[ 0,  1,  2, ...,  0],
+    #  [ 1,  2,  3, ...,  10],
+    #  [...],
+    #  [8,  9,  10, ...,  17]]
+    #  [9, 10,  11, ...,  18]]
+    return xr.DataArray(
+        data=np.arange(10) + np.arange(10).reshape(-1, 1),
+        dims=("row", "col"),
+        coords={"row": np.arange(10), "col": np.arange(10)},
+    )
+
+
+@pytest.fixture()
+def right_data(left_data):
+    return left_data + 1
+
+
+@pytest.fixture()
+def transform():
+    return rasterio.Affine(0.5, 0.0, 573083.5, 0.0, -0.5, 4825333.5)
+
+
+@pytest.fixture()
+def crs():
+    return rasterio.crs.CRS.from_epsg(32631)
+
+
+@pytest.fixture()
+def left_path(tmp_path, left_data, crs, transform):
+    """Write left image and return its path."""
+    path = tmp_path / "left.tif"
+    write_data_array(
+        data_array=left_data,
+        filename=str(path),
+        crs=crs,
+        transform=transform,
+    )
+    return path
+
+
+@pytest.fixture()
+def right_path(tmp_path, right_data, crs, transform):
+    """Write right image and return its path."""
+    path = tmp_path / "right.tif"
+    write_data_array(
+        data_array=right_data,
+        filename=str(path),
+        crs=crs,
+        transform=transform,
+    )
+    return path
+
+
+@pytest.fixture()
+def configuration(left_path, right_path, correct_pipeline_without_refinement, step, tmp_path):
+    correct_pipeline_without_refinement["pipeline"]["matching_cost"]["step"] = step
+    return {
+        "input": {
+            "left": {
+                "img": str(left_path),
+            },
+            "right": {
+                "img": str(right_path),
+            },
+            "col_disparity": {"init": 1, "range": 2},
+            "row_disparity": {"init": 1, "range": 2},
+        },
+        **correct_pipeline_without_refinement,
+        "output": {"path": str(tmp_path.absolute())},
     }
