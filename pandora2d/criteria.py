f5a003407ca1a4466aaed369d0aebb6f57a3aabb
Marie Leconte <marie.leconte@csgroup.eu>
2025-09-05 14:46:44 +0200

    fix: update coordinate when we use ROI and right mask
    When working with an ROI and a step, the left/right images
    and the corresponding cost_volume/criteria_dataarray may not share the same indexes.
    As a result, the subtraction operation for the mask in the apply_right_criteria_mask
    method must account for differences in image size.

--- pandora2d/criteria.py
+++ pandora2d/criteria.py
@@ -288,13 +288,13 @@ def mask_right_no_data(img_right: xr.Dataset, window_size: int, criteria_dataarr
     :param criteria_dataarray:
     :type criteria_dataarray:
     """
-    right_criteria_mask = np.full_like(img_right["msk"], Criteria.VALID, dtype=np.uint8)
+    mask_criteria_right = xr.full_like(img_right["msk"], Criteria.VALID, dtype=np.uint8)
 
     right_binary_mask = binary_dilation_dataarray(img_right, window_size)
     # With in place operation we need to cast Criteria (seen as int64). Seems to be related to unsigned.
-    right_criteria_mask[right_binary_mask] |= np.uint8(Criteria.P2D_RIGHT_NODATA)
+    mask_criteria_right.data[right_binary_mask] |= np.uint8(Criteria.P2D_RIGHT_NODATA)
 
-    apply_right_criteria_mask(criteria_dataarray, right_criteria_mask)
+    apply_right_criteria_mask(criteria_dataarray, mask_criteria_right)
 
 
 def mask_left_invalid(left_image: xr.Dataset, criteria_dataarray: xr.DataArray) -> None:
@@ -315,27 +315,27 @@ def mask_left_invalid(left_image: xr.Dataset, criteria_dataarray: xr.DataArray) 
     criteria_dataarray.data[invalid_left_mask.data, ...] |= np.uint8(Criteria.P2D_INVALID_MASK_LEFT)
 
 
-def mask_right_invalid(right_image: xr.Dataset, criteria_dataarray: xr.DataArray) -> None:
+def mask_right_invalid(img_right: xr.Dataset, criteria_dataarray: xr.DataArray) -> None:
     """
     This method raises P2D_INVALID_MASK_RIGHT criteria for points having
     an invalid point in the right image mask shift by its disparity.
     A point is considered invalid if when we shift it by its disparity, the obtained value
     is different from the values of the valid_pixels and no_data_mask attributes.
 
-    :param right_image: right image with `msk` data var.
-    :type right_image: xr.Dataset
+    :param img_right: right image with `msk` data var.
+    :type img_right: xr.Dataset
     :param criteria_dataaray: criteria dataarray to update
     :type criteria_dataaray: xr.DataArray
     """
 
-    right_criteria_mask = np.full_like(right_image["msk"], Criteria.VALID, dtype=np.uint8)
+    mask_criteria_right = xr.full_like(img_right["msk"], Criteria.VALID, dtype=np.uint8)
 
-    invalid_right_mask = get_invalid_mask(right_image)
+    invalid_right_mask = get_invalid_mask(img_right)
 
     # With in place operation we need to cast Criteria (seen as int64). Seems to be related to unsigned.
-    right_criteria_mask[invalid_right_mask.data] |= np.uint8(Criteria.P2D_INVALID_MASK_RIGHT)
+    mask_criteria_right.data[invalid_right_mask.data] |= np.uint8(Criteria.P2D_INVALID_MASK_RIGHT)
 
-    apply_right_criteria_mask(criteria_dataarray, right_criteria_mask)
+    apply_right_criteria_mask(criteria_dataarray, mask_criteria_right)
 
 
 def get_invalid_mask(image: xr.Dataset) -> xr.DataArray:
@@ -354,19 +354,19 @@ def get_invalid_mask(image: xr.Dataset) -> xr.DataArray:
     return invalid_mask
 
 
-def apply_right_criteria_mask(criteria_dataarray: xr.DataArray, right_criteria_mask: NDArray):
+def apply_right_criteria_mask(criteria_dataarray: xr.DataArray, mask_criteria_right: xr.DataArray):
     """
-    This method apply right_criteria_mask array on criteria_dataarray according
+    This method apply mask_criteria_right array on criteria_dataarray according
     to row and column disparities.
 
     :param criteria_dataaray: criteria dataarray to update
     :type criteria_dataaray: xr.DataArray
-    :param right_criteria_mask: mask to apply to criteria dataarray
-    :type right_criteria_mask: np.NDArray
+    :param mask_criteria_right: mask to apply to criteria dataarray
+    :type mask_criteria_right: xr.DataArray
     """
 
     # We use a temporary mask of the same size as the image to correctly handle cases where the step is different from 1
-    mask_img_shape = np.zeros_like(right_criteria_mask, dtype=np.uint8)
+    mask_img_shape = np.zeros_like(mask_criteria_right, dtype=np.uint8)
 
     for row_disp, col_disp in itertools.product(
         criteria_dataarray.coords["disp_row"], criteria_dataarray.coords["disp_col"]
@@ -391,7 +391,7 @@ def apply_right_criteria_mask(criteria_dataarray: xr.DataArray, right_criteria_m
         criteria_col_slice = np.s_[-col_dsp_int:] if col_dsp_int <= 0 else np.s_[:-col_dsp_int]  # type: ignore[index]
 
         mask_img_shape[:] = 0
-        mask_img_shape[criteria_row_slice, criteria_col_slice] |= right_criteria_mask[msk_row_slice, msk_col_slice]
+        mask_img_shape[criteria_row_slice, criteria_col_slice] |= mask_criteria_right[msk_row_slice, msk_col_slice]
 
         # We subtract the first row and col coordinates to keep the correct indexes when processing a ROI.
         criteria_dataarray.loc[
@@ -400,8 +400,8 @@ def apply_right_criteria_mask(criteria_dataarray: xr.DataArray, right_criteria_m
                 "disp_col": col_disp,
             }
         ] |= mask_img_shape[
-            criteria_dataarray.coords["row"].data[:, None] - criteria_dataarray.coords["row"].data[0],
-            criteria_dataarray.coords["col"].data[None, :] - criteria_dataarray.coords["col"].data[0],
+            criteria_dataarray.coords["row"].data[:, None] - mask_criteria_right.coords["row"].data[0],
+            criteria_dataarray.coords["col"].data[None, :] - mask_criteria_right.coords["col"].data[0],
         ]
 
 
