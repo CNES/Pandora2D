a7ec72aa6fdd3778ec79e36f81682b3d64d87953
Marie Leconte <marie.leconte@csgroup.eu>
2026-04-10 14:37:52 +0200

    test: mv TestCostVolumeConfidence class

--- tests/functional_tests/test_cost_volume_confidence.py
+++ tests/functional_tests/test_cost_volume_confidence.py
@@ -25,6 +25,7 @@ Functional tests for configurations with cost_volume_confidence step.
 import copy
 import numpy as np
 import pytest
+import rasterio
 
 import pandora2d
 from pandora2d.check_configuration import check_conf
@@ -118,3 +119,47 @@ class TestAmbiguity:
         # Checking that resulting confidence_measure map are not full of 0 or same value (confidence_data.flat[0])
         assert not np.all(confidence_data == 0)
         assert not np.all(confidence_data == confidence_data.flat[0])
+
+
+class TestCostVolumeConfidence:
+    """
+    Test cost volume confidence execution
+    """
+
+    @pytest.fixture()
+    def pipeline_cfg_with_confidence(self):
+        """
+        Cost volume confidence configuration
+        """
+
+        return {
+            "pipeline": {
+                "matching_cost": {
+                    "matching_cost_method": "zncc",
+                    "window_size": 5,
+                },
+                "cost_volume_confidence": {"confidence_method": "ambiguity", "eta_max": 0.7},
+                "disparity": {"disparity_method": "wta", "invalid_disparity": -99},
+            }
+        }
+
+    @pytest.fixture()
+    def configuration(self, correct_input_cfg, pipeline_cfg_with_confidence, tmp_path):
+        return {
+            **correct_input_cfg,
+            **pipeline_cfg_with_confidence,
+            **{"output": {"path": str(tmp_path)}},
+        }
+
+    def test_cost_volume_confidence_pipeline(self, configuration, run_pipeline, tmp_path):
+        """
+        Test execution of a pipeline with cost volumes confidence
+        """
+
+        run_pipeline(configuration)
+
+        with rasterio.open(tmp_path / "cost_volumes" / "confidence_measure.tif") as src:
+            confidence_map = src.read(1)
+
+        # Checking that resulting confidence map is not full of nans
+        assert not np.all(np.isnan(confidence_map))
